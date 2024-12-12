3e38d5aebfa837114eb0fd99df3150454af621ce
Taylor Wong <taylorwong@Taylors-MacBook-Air.local>
2024-12-11 20:46:27 -0500

    Started home page

---
+++ EventFinder.js
@@ -0,0 +1,9 @@
+function changeHomePage(){
+    window.location.href = "Home.html"
+}
+function changeAboutPage(){
+    window.location.href = "About.html"
+}
+function changeEventsPage(){
+    window.location.href = "Events.html"
+}
